dd542498b503628b8f87bb70a97f78cd42307a82
jonathan-jipslok <jonathanpoli17@gmail.com>
2026-04-22 20:26:43 -0300

    Novo projeto, tela de cards de produtos

---
+++ UNIDADE 03/Projeto Livraria/js/module/carregarDados.js
@@ -0,0 +1,11 @@
+/**************************************************************************************************
+ * Objetivo: Criar a comunicação entre a página HTML e os dados fornecidos de livros 
+ * (arquivo de dados ou API)
+ * Data: 22/04/2026
+ * Autor: Jonathan Yuri Poli Siqueira
+ * Versão: 1.0
+ *************************************************************************************************/
+
+import { livros } from "./livros.js";
+
+console.log(livros);

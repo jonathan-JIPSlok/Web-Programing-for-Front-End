45a364f6c84b5f902fe77afe1ff94af29562561e
jonathan-jipslok <jonathanpoli17@gmail.com>
2026-04-24 21:52:38 -0300

    Comaçando a consumir uma API

---
+++ UNIDADE 04/admin_livros/js/crud.js
@@ -0,0 +1,37 @@
+/****************************************************************************************************************
+ * Objetivo: Realizar o CRUD de dados referente aos livros
+ * Data: 24/04/2026
+ * Autor: Jonathan Yuri Poli Siqueira
+ * Versão: 1.0
+ * API: https://app-livraria-2024-gsc9e3gcdsh2f2b5.brazilsouth-01.azurewebsites.net/v2/livraria/livros
+ *****************************************************************************************************************/
+
+
+//Inserir um novo livro
+const postLivro = async function() {
+
+}
+
+
+//Atualizar um livro existente
+const putLivro = async function() {
+
+}
+
+
+//excluir um livro existente
+const deleteLivro = async function() {
+
+}
+
+
+//buscar todos os livros cadastrados
+const getAllLivros = async function() {
+
+}
+
+
+//buscar um livro pelo id
+const getByIdLivro = async function() {
+
+}

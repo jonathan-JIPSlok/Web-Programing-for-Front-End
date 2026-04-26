58d7ba7465fd415d4f216ddd8e1bf91a8365300b
jonathan-jipslok <jonathanpoli17@gmail.com>
2026-04-26 00:43:25 -0300

    feat: Recebendo os livros da API e adicionando a tabela

--- UNIDADE 04/admin_livros/js/crud.js
+++ UNIDADE 04/admin_livros/js/crud.js
@@ -46,6 +46,9 @@ const postLivro = async function() {
 
     if(response.status == 201) {
         alert('Livro cadastrado com sucesso!');
+        resetForm();
+        getAllLivros();
+        
     }else{
         alert('Não foi possível cadastrar o livro. Houve problemas na requisição.');
     }
@@ -67,6 +70,62 @@ const deleteLivro = async function() {
 
 //buscar todos os livros cadastrados
 const getAllLivros = async function() {
+    let URL = 'https://app-livraria-2024-gsc9e3gcdsh2f2b5.brazilsouth-01.azurewebsites.net/v2/livraria/livros';
+
+    let response = await fetch(URL)
+
+    let dados = await response.json();
+
+    //Recebe o elemento principal para colocar a lista de dados
+    let divListDados = document.getElementById('listDados');
+
+    divListDados.innerText = ''; //Limpa os dados para evitar duplicidade
+
+
+    dados.book.forEach(function(itemLivro){
+        let nomeLivro = itemLivro.title;
+        let descLivro = itemLivro.subtitle;
+        let valorLivro = itemLivro.price;
+        let idLivro = itemLivro.id;
+        
+
+        let nomeLivros = 'Nome'
+        let descLivros = 'Descrição'
+        let valorLivros = '10'
+
+        
+        //Criar elementos no HTML
+        let divDados = document.createElement('div');
+        let divTitle = document.createElement('div');
+        let divSubtitle = document.createElement('div');
+        let divPrice = document.createElement('div');
+        let divOpcoes = document.createElement('div');
+        let spanEditar = document.createElement('span');
+        let spanExcluir = document.createElement('span');
+        let imgEditar = document.createElement('img');
+        let imgExcluir = document.createElement('img');
+
+        //Adicionar atributos
+        divDados.setAttribute('class', 'linha dados');
+        imgEditar.setAttribute('src', "icones/ditar.png");
+        imgExcluir.setAttribute('src', "icones/excluir.png");
+
+        //Textos
+        divTitle.innerText = nomeLivros;
+        divSubtitle.innerText = descLivros;
+        divPrice.innerText = valorLivros;
+
+        //Associar os elementos criados com o elemento principal
+        divListDados.appendChild(divDados);
+        divDados.appendChild(divTitle);
+        divDados.appendChild(divSubtitle);
+        divDados.appendChild(divPrice);
+        divDados.appendChild(divOpcoes);
+        divOpcoes.appendChild(spanEditar);
+        divOpcoes.appendChild(spanExcluir);
+        spanEditar.appendChild(imgEditar);
+        spanExcluir.appendChild(imgExcluir);
+    });
 
 }
 
@@ -77,6 +136,20 @@ const getByIdLivro = async function() {
 }
 
 
+//Limpar os campos do formulário
+const resetForm = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('subtitle').value = '';
+    document.getElementById('image').value = '';
+    document.getElementById('price').value = '';
+}
+
+
 botaoSalvar.addEventListener('click', function() {
     postLivro();
+});
+
+
+window.addEventListener('load', function() {
+    getAllLivros();
 });

52d5705e9b597adcfd6d27dd430e3728789e7931
jonathan-jipslok <jonathanpoli17@gmail.com>
2026-04-26 00:07:42 -0300

    feat: Cadastrando produtos na API

--- UNIDADE 04/admin_livros/js/crud.js
+++ UNIDADE 04/admin_livros/js/crud.js
@@ -7,8 +7,48 @@
  *****************************************************************************************************************/
 
 
+const botaoSalvar = document.getElementById('salvar');
+
+
 //Inserir um novo livro
 const postLivro = async function() {
+
+    let URL = 'https://app-livraria-2024-gsc9e3gcdsh2f2b5.brazilsouth-01.azurewebsites.net/v2/livraria/livro';
+
+    let dadosJSON = {};
+
+    //Receber os dados do formulário
+    let nomeLivro       = document.getElementById('title');
+    let descricaoLivro  = document.getElementById('subtitle');
+    let fotoLivro       = document.getElementById('image');
+    let valorLivro      = document.getElementById('price');
+
+    //Validação
+
+
+
+    //Crira o JSON de dados
+    dadosJSON.title     = nomeLivro.value;
+    dadosJSON.subtitle  = descricaoLivro.value;
+    dadosJSON.image     = fotoLivro.value;
+    dadosJSON.price     = valorLivro.value;
+
+    console.log(dadosJSON);
+    //POST dos dados para a API de Livros
+    let response = await fetch(URL, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(dadosJSON)
+    })
+
+    if(response.status == 201) {
+        alert('Livro cadastrado com sucesso!');
+    }else{
+        alert('Não foi possível cadastrar o livro. Houve problemas na requisição.');
+    }
 
 }
 
@@ -35,3 +75,8 @@ const getAllLivros = async function() {
 const getByIdLivro = async function() {
 
 }
+
+
+botaoSalvar.addEventListener('click', function() {
+    postLivro();
+});
